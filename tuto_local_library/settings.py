39e5dfa20a097aa273ba2013807c3c0c5594ed36
syshinkr <syshinkr228@gmail.com>
2021-10-30 13:52:52 +0900

    Part 5: Creating our home page

--- tuto_local_library/settings.py
+++ tuto_local_library/settings.py
@@ -52,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tuto_local_library.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
